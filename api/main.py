68d1f1cb4014d0a954a4806ece0deeed6661061c
Elias Safo <eliassafo@gmail.com>
2024-05-16 13:22:30 +0200

    Added corsmiddleware

--- api/main.py
+++ api/main.py
@@ -5,7 +5,6 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-# Configure CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:3000"],  # Replace with your frontend URL(s)
